7421a9041f6c3f543229e16ccd85b3b077522964
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-08-18 15:51:47 -0400

    move loop into try_copy to keep reading data while not blocked

--- src/tcp.rs
+++ src/tcp.rs
@@ -41,44 +41,44 @@ struct BufState {
 }
 
 impl BufState {
-    fn try_write(
+    fn try_copy(
         &mut self,
         reader: &mut TcpStream,
         writer: &mut TcpStream,
     ) -> Poll<u64, io::Error> {
-        // If buffer is empty: read some data
-        if self.pos == self.cap && !self.read_done {
-            let n = try_ready!(reader.poll_read(&mut self.buf));
-            if n == 0 {
-                self.read_done = true;
-            } else {
-                self.pos = 0;
-                self.cap = n;
+        loop {
+            // If buffer is empty: read some data
+            if self.pos == self.cap && !self.read_done {
+                let n = try_ready!(reader.poll_read(&mut self.buf));
+                if n == 0 {
+                    self.read_done = true;
+                } else {
+                    self.pos = 0;
+                    self.cap = n;
+                }
+            }
+
+            // If buffer has data: write it out
+            while self.pos < self.cap {
+                let i = try_ready!(writer.poll_write(&self.buf[self.pos..self.cap]));
+                if i == 0 {
+                    return Err(io::Error::new(
+                        io::ErrorKind::WriteZero,
+                        "writer accepted zero bytes",
+                    ));
+                } else {
+                    self.pos += i;
+                    self.amt += i as u64;
+                }
+            }
+
+            // If transfer done: flush out data and shutdown the destination stream
+            if self.pos == self.cap && self.read_done {
+                try_ready!(writer.poll_flush());
+                TcpStream::shutdown(writer, Shutdown::Write)?;
+                return Ok(self.amt.into());
             }
         }
-
-        // If buffer has data: write it out
-        while self.pos < self.cap {
-            let i = try_ready!(writer.poll_write(&self.buf[self.pos..self.cap]));
-            if i == 0 {
-                return Err(io::Error::new(
-                    io::ErrorKind::WriteZero,
-                    "writer accepted zero bytes",
-                ));
-            } else {
-                self.pos += i;
-                self.amt += i as u64;
-            }
-        }
-
-        // If transfer done: flush out data and shutdown the destination stream
-        if self.pos == self.cap && self.read_done {
-            try_ready!(writer.poll_flush());
-            TcpStream::shutdown(writer, Shutdown::Write)?;
-            return Ok(self.amt.into());
-        }
-
-        Ok(Async::NotReady)
     }
 }
 
@@ -87,11 +87,10 @@ impl Future for Conjoin {
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        loop {
-            let a_to_b = self.a_to_b.try_write(&mut self.a, &mut self.b);
-            let b_to_a = self.b_to_a.try_write(&mut self.b, &mut self.a);
-            // once both transfers are done, return transferred bytes
-            return Ok((try_ready!(a_to_b), try_ready!(b_to_a)).into());
-        }
+        // always attempt transfers in both directions
+        let a_to_b = self.a_to_b.try_copy(&mut self.a, &mut self.b);
+        let b_to_a = self.b_to_a.try_copy(&mut self.b, &mut self.a);
+        // once both transfers are done, return transferred bytes
+        Ok((try_ready!(a_to_b), try_ready!(b_to_a)).into())
     }
 }
