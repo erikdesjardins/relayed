cd9a628f5b7ac67cc6764b0604d905bde38cd2e9
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-08-18 21:55:32 -0400

    improve docs

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # nproxy
 
 Proxy traffic to a machine behind a dynamic IP/firewall. Simpler and less secure than a VPN.
-
-## Example
-
-On a publicly-exposed machine (IP `1.2.3.4`):
-
-```bash
-nproxy server 0.0.0.0:8080 0.0.0.0:3000
-```
-
-On the private machine:
-
-```bash
-nproxy client 1.2.3.4:3000 127.0.0.1:80
-```
-
-Which will proxy TCP from `1.2.3.4:8080` to `localhost:80` on the private machine.
--- src/opt.rs
+++ src/opt.rs
@@ -30,6 +30,9 @@ impl FromStr for Mode {
 }
 
 /// Proxy traffic to a machine behind a dynamic IP/firewall.
+/// Sockets can be IPv4 (`0.0.0.0:80`) or IPv6 (`[1:2:3:4::]:80`).
+///
+/// EXAMPLE: `server 0.0.0.0:8080 0.0.0.0:3000` `client 1.2.3.4:3000 127.0.0.1:80`
 #[derive(StructOpt, Debug)]
 pub struct Options {
     /// Logging verbosity (-v info, -vv debug, -vvv trace)
@@ -40,12 +43,12 @@ pub struct Options {
     #[structopt(short = "r", long = "retry")]
     pub retry: bool,
 
-    /// `server` or `client` mode
+    /// `server` or `client`
     pub mode: Mode,
 
-    /// For servers, the socket to proxy from; for clients, the server to connect to
+    /// For servers, the socket to proxy from; for clients, the server's gateway socket
     pub from: SocketAddr,
 
-    /// For servers, the internal socket for the client; for clients, the socket to proxy to
+    /// For servers, the gateway socket;       for clients, the socket to proxy to
     pub to: SocketAddr,
 }
