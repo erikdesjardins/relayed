cc4d584668d190bc3f648cfb24afb67238069f89
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2019-05-18 17:12:56 -0400

    server: put gateway first to match order of cli arguments

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,9 @@ fn main() -> Result<(), err::DebugFromDisplay<std::io::Error>> {
         .init();
 
     match mode {
-        opt::Mode::Server { public, gateway } => server::run(
-            &([0, 0, 0, 0], public).into(),
+        opt::Mode::Server { gateway, public } => server::run(
             &([0, 0, 0, 0], gateway).into(),
+            &([0, 0, 0, 0], public).into(),
         )?,
         opt::Mode::Client {
             gateway,
