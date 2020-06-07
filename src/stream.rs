e35e30129f0f50472b4179c4833f00e5aaac9802
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2020-06-07 12:49:11 -0400

    handle dropped request tokens (instead of deadlocking)

--- src/stream.rs
+++ src/stream.rs
@@ -4,55 +4,53 @@ use pin_utils::pin_mut;
 use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 use tokio::task::LocalSet;
 
 /// Spawns a stream onto the local set to perform idle work.
 /// This keeps polling the inner stream even when no item is demanded by the parent,
 /// allowing it to keep making progress.
-pub fn spawn_idle<T, S>(local: &LocalSet, f: impl FnOnce(Requests) -> S) -> impl Stream<Item = T>
+pub fn spawn_idle<T, S>(local: &LocalSet, f: impl FnOnce(Requests<T>) -> S) -> impl Stream<Item = T>
 where
     T: 'static,
-    S: Stream<Item = (RequestToken, T)> + 'static,
+    S: Stream<Item = (RequestToken<T>, T)> + 'static,
 {
     let (request, requests) = mpsc::channel(1);
-    let (mut response, responses) = mpsc::channel(1);
 
     let idle = f(Requests(requests));
     local.spawn_local(async move {
         pin_mut!(idle);
         loop {
             match idle.next().await {
-                Some(resp) => match response.send(resp).await {
+                Some((RequestToken(chan), val)) => match chan.send(val) {
                     Ok(()) => continue,
-                    Err(mpsc::error::SendError(_)) => return,
+                    Err(_) => return,
                 },
                 None => return,
             }
         }
     });
 
-    stream::unfold(
-        (request, responses, RequestToken(())),
-        |(mut request, mut responses, token)| async {
-            match request.send(token).await {
-                Ok(()) => match responses.recv().await {
-                    Some((token, val)) => Some((val, (request, responses, token))),
-                    None => None,
+    stream::unfold(request, |mut request| async {
+        loop {
+            let (respond, response) = oneshot::channel();
+            match request.send(RequestToken(respond)).await {
+                Ok(()) => match response.await {
+                    Ok(val) => return Some((val, request)),
+                    Err(_) => continue,
                 },
-                Err(mpsc::error::SendError(_)) => None,
+                Err(mpsc::error::SendError(_)) => return None,
             }
-        },
-    )
+        }
+    })
 }
 
-/// Unclonable token proving that a request was sent.
-pub struct RequestToken(());
+pub struct RequestToken<T>(oneshot::Sender<T>);
 
-pub struct Requests(mpsc::Receiver<RequestToken>);
+pub struct Requests<T>(mpsc::Receiver<RequestToken<T>>);
 
-impl Stream for Requests {
-    type Item = RequestToken;
+impl<T> Stream for Requests<T> {
+    type Item = RequestToken<T>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         Stream::poll_next(Pin::new(&mut self.0), cx)
