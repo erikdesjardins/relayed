3f31622409af71e362df116ffbc481db3744e10c
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2020-06-07 13:58:00 -0400

    actually, panic on dropped tokens instead (so we can use zst tokens again, and because it was necessary to reorder things this way due to previous commit)
    re e35e30129f0f50472b4179c4833f00e5aaac9802 (HEAD~3)

--- src/stream.rs
+++ src/stream.rs
@@ -1,58 +1,66 @@
 use futures::stream;
 use futures::{Stream, StreamExt};
 use pin_utils::pin_mut;
+use std::mem::ManuallyDrop;
 use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
-use tokio::sync::{mpsc, oneshot};
+use tokio::sync::mpsc;
 use tokio::task::LocalSet;
 
 /// Spawns a stream onto the local set to perform idle work.
 /// This keeps polling the inner stream even when no item is demanded by the parent,
 /// allowing it to keep making progress.
-pub fn spawn_idle<T, S>(local: &LocalSet, f: impl FnOnce(Requests<T>) -> S) -> impl Stream<Item = T>
+pub fn spawn_idle<T, S>(local: &LocalSet, f: impl FnOnce(Requests) -> S) -> impl Stream<Item = T>
 where
     T: 'static,
-    S: Stream<Item = (RequestToken<T>, T)> + 'static,
+    S: Stream<Item = (RequestToken, T)> + 'static,
 {
     let (request, requests) = mpsc::channel(1);
+    let (mut response, responses) = mpsc::channel(1);
 
     let idle = f(Requests(requests));
     local.spawn_local(async move {
         pin_mut!(idle);
         loop {
             match idle.next().await {
-                Some((RequestToken(chan), val)) => match chan.send(val) {
+                Some((token, val)) => match response.send((ManuallyDrop::new(token), val)).await {
                     Ok(()) => continue,
-                    Err(_) => return,
+                    Err(mpsc::error::SendError(_)) => return,
                 },
                 None => return,
             }
         }
     });
 
-    stream::unfold(request, |mut request| async {
-        loop {
-            let (respond, response) = oneshot::channel();
-            match request.send(RequestToken(respond)).await {
-                Ok(()) => match response.await {
-                    Ok(val) => return Some((val, request)),
-                    Err(_) => continue,
+    stream::unfold(
+        (request, responses, ManuallyDrop::new(RequestToken(()))),
+        |(mut request, mut responses, token)| async {
+            match request.send(token).await {
+                Ok(()) => match responses.recv().await {
+                    Some((token, val)) => Some((val, (request, responses, token))),
+                    None => None,
                 },
-                Err(mpsc::error::SendError(_)) => return None,
+                Err(mpsc::error::SendError(_)) => None,
             }
-        }
-    })
+        },
+    )
 }
 
-pub struct RequestToken<T>(oneshot::Sender<T>);
+pub struct RequestToken(());
 
-pub struct Requests<T>(mpsc::Receiver<RequestToken<T>>);
+impl Drop for RequestToken {
+    fn drop(&mut self) {
+        panic!("Deadlock: request token dropped");
+    }
+}
 
-impl<T> Stream for Requests<T> {
-    type Item = RequestToken<T>;
+pub struct Requests(mpsc::Receiver<ManuallyDrop<RequestToken>>);
+
+impl Stream for Requests {
+    type Item = RequestToken;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        Stream::poll_next(Pin::new(&mut self.0), cx)
+        Stream::poll_next(Pin::new(&mut self.0), cx).map(|p| p.map(ManuallyDrop::into_inner))
     }
 }
